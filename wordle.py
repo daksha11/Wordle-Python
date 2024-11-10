837630feeae4ad26f9398c4af2eff7b74038d848
Daksha Rongala <rongala.daksha11@gmail.com>
2024-11-10 17:33:21 +0530

    Inital Commit

---
+++ wordle.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import random
+
+df = pd.read_csv('./5_letters.csv')
+random_number = random.randint(0, len(df))
+word = ""
+for i in range(5):
+    word = word + df.iloc[random_number][i]
+word = word.upper()
+word_array = list(word)
+chances = 6
+
+while (chances > 0):
+    word_dict = {}
+    for i in range(5):
+        if (word_dict.get(word_array[i]) == None):
+            word_dict.setdefault(word_array[i], 1)
+        else:
+            word_dict.update({word_array[i] : word_dict.get(word_array[i]) + 1})
+
+    string = input("Enter a word: ").upper()
+    
+    if (len(string) != 5):
+        print("Enter a 5 letter word.")
+        print()
+        continue
+
+    print()
+    string_array = list(string)
+    print(string_array)
+
+    check = []
+    correct_count = 0
+    for i in range(5):
+        if (word_array[i] == string_array[i]):
+            check.append('✔')
+            correct_count = correct_count + 1
+            word_dict.update({word_array[i] : word_dict.get(word_array[i]) - 1})
+        else:
+            check.append('X')
+
+    for i in range(5):
+        if (check[i] == 'X'):
+            if (string_array[i] not in word_dict):
+                check[i] = "X"
+            else:
+                count = word_dict.get(string_array[i]) - 1
+
+                if (count < 0):
+                    check[i] = "X"
+                else:
+                    check[i] = "O"
+                    word_dict.update({string_array[i] : count})
+    print(check)
+
+    if (correct_count == 5):
+        break
+
+    chances = chances - 1
+    print("Chances Remaining:", chances)
+    print()
+
+if(chances == 0):
+    print(word)
+    print("You Lose.")
+else:
+    print()
+    print("You Win.")
